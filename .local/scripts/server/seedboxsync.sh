7e8d27c68da463a6447fabf138627d42bf2894a3
John Ramsden <johnramsden@riseup.net>
2018-03-13 17:25:50 -0700

    Allow run if one type of sync fails

--- .local/scripts/server/seedboxsync.sh
+++ .local/scripts/server/seedboxsync.sh
@@ -14,7 +14,6 @@ FILES_OWNER="media"
 FILES_GROUP="media"
 
 seedbox_pull_seeding() {
-      SYNC_TIMESTAMP="$(date "+%F_%H_%M_%S" )"
 
       remote_source="${1}"
       local_destination="${2}"
@@ -22,14 +21,14 @@ seedbox_pull_seeding() {
       mkdir -p "${local_destination}" && chown ${FILES_OWNER}:${FILES_GROUP} "${local_destination}" || return 1
 
       rsync --recursive \
+            --verbose \
             --delete \
             -og --chown=${FILES_OWNER}:${FILES_GROUP} \
-            -e ssh "${SEEDBOX_USER}@${SEEDBOX_SERVER}:${remote_source}" \
-                  "${local_destination}" || \
-      echo "rsync ${SYNC_TIMESTAMP} had no changes" && return 0
+            "${SEEDBOX_USER}@${SEEDBOX_SERVER}:${remote_source}/" "${local_destination}" || \
+      echo "rsync ${remote_source} had no changes" && return 1
 
       echo
-      echo "rsync ${SYNC_TIMESTAMP} seccessful from ${SEEDBOX_USER}@${SEEDBOX_SERVER}"
+      echo "rsync ${remote_source} seccessful from ${SEEDBOX_USER}@${SEEDBOX_SERVER}"
       echo
       echo "Moving files to ${COMPLETED_SYNC_LOCATION}"
 
@@ -51,8 +50,7 @@ seedbox_pull_downloaded() {
             --delete \
             -og --chown=${FILES_OWNER}:${FILES_GROUP} \
             --remove-source-files \
-            -e ssh "${SEEDBOX_USER}@${SEEDBOX_SERVER}:${remote_source}" \
-                  "${local_destination}/${SYNC_TIMESTAMP}" || \
+            "${SEEDBOX_USER}@${SEEDBOX_SERVER}:${remote_source}" "${local_destination}/${SYNC_TIMESTAMP}" || \
       echo "rsync ${SYNC_TIMESTAMP} failed" && return 1
 
       echo
@@ -72,11 +70,28 @@ seedbox_pull_downloaded() {
 # remote_source="${1}" - remote location
 # local_destination="${2}" - local destination
 
-seedbox_pull_downloaded "${SEEDBOX_DELUGE_ROOT}/${SEEDBOX_DOWNLOADING_DIR}/" "${SYNC_LOCATION}" || \
-echo "Failed sync downloaded files" && exit 1
+echo "Running sync of downloaded ${SEEDBOX_DELUGE_ROOT}/${SEEDBOX_DOWNLOADING_DIR}"
+seedbox_pull_downloaded "${SEEDBOX_DELUGE_ROOT}/${SEEDBOX_DOWNLOADING_DIR}/" "${SYNC_LOCATION}"
+downloaded_success=$?
 
-seedbox_pull_seeding "${SEEDBOX_DELUGE_ROOT}/${SEEDBOX_SEEDING_DIR}" "${SYNC_LOCATION}/seeding" || \
-echo "Failed sync seeding files" && exit 1
+echo
+echo "Running sync of seeding ${SEEDBOX_DELUGE_ROOT}/${SEEDBOX_SEEDING_DIR}"
+seedbox_pull_seeding "${SEEDBOX_DELUGE_ROOT}/${SEEDBOX_SEEDING_DIR}" "${SYNC_LOCATION}/seeding"
+seeding_success=$?
 
-echo "Successful sync from ${SEEDBOX_USER}@${SEEDBOX_SERVER}"
-exit 0
+echo
+
+if [ ${downloaded_success} -eq 0 ] && [ ${seeding_success} -eq 0 ]; then
+      echo "Successful sync from ${SEEDBOX_USER}@${SEEDBOX_SERVER}"
+      exit 0
+else
+      if [ ${downloaded_success} -ne 0 ]; then
+            echo "Failed sync downloaded files, or no changes on source"
+      fi
+
+      if [ ${seeding_success} -ne 0 ]; then
+            echo "Failed sync seeding files, or no changes on source"
+      fi
+
+      exit 1
+fi

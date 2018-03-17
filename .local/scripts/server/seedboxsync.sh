ac440a5047b67177acbce98bc0f8eb27ec04360b
John Ramsden <johnramsden@riseup.net>
2018-03-16 18:26:49 -0700

    Use lockf to lock rsync destination

--- .local/scripts/server/seedboxsync.sh
+++ .local/scripts/server/seedboxsync.sh
@@ -4,94 +4,93 @@ SEEDBOX_USER="cacheline"
 SEEDBOX_SERVER="polyphemus.feralhosting.com"
 
 SEEDBOX_DELUGE_ROOT="/media/sdl1/cacheline/private/deluge"
+
 SEEDBOX_SEEDING_DIR="complete/seeding"
 SEEDBOX_DOWNLOADING_DIR="complete/downloading"
 
-SYNC_LOCATION="/mnt/tank/media/Downloads/Incomplete/box"
-COMPLETED_SYNC_LOCATION="/mnt/tank/media/Downloads/Complete"
+SYNC_LOCATION="/mnt/tank/media/Downloads/Incomplete/seedbox"
+COMPLETED_SYNC_LOCATION="/mnt/tank/media/Downloads/Complete/downloading"
 
 FILES_OWNER="media"
 FILES_GROUP="media"
+
+seedbox_pull_downloaded() {
+      sync_timestamp="$(date "+%F_%H_%M_%S" )"
+
+      remote_source="${1}"
+      local_destination="${2}"
+
+      mkdir -p ${local_destination}/${sync_timestamp} && chown ${FILES_OWNER}:${FILES_GROUP} ${local_destination}/${sync_timestamp}
+
+      /usr/bin/lockf -s -t 0 -k "${local_destination}/${sync_timestamp}" \
+            /usr/local/bin/rsync \
+                  --times \
+                  --verbose \
+                  --compress \
+                  --recursive \
+                  --human-readable \
+                  --remove-source-files \
+                  -og --chown=${FILES_OWNER}:${FILES_GROUP} \
+                        "${SEEDBOX_USER}@${SEEDBOX_SERVER}:${remote_source}" "${local_destination}/${sync_timestamp}"
+
+      if [ ${?} -ne 0 ]; then
+            echo "rsync ${sync_timestamp} failed" && return 1
+      fi      
+
+      echo
+      echo "rsync ${remote_source} --> ${local_destination}/${sync_timestamp} successful from ${SEEDBOX_USER}@${SEEDBOX_SERVER}"
+      echo
+
+      return 0     
+}
 
 seedbox_pull_seeding() {
 
       remote_source="${1}"
       local_destination="${2}"
 
-      mkdir -p "${local_destination}" && chown ${FILES_OWNER}:${FILES_GROUP} "${local_destination}" || return 1
-
-      rsync --recursive \
-            --verbose \
-            --delete \
-            -og --chown=${FILES_OWNER}:${FILES_GROUP} \
-            "${SEEDBOX_USER}@${SEEDBOX_SERVER}:${remote_source}/" "${local_destination}"
+      /usr/bin/lockf -s -t 0 -k "${local_destination}" \
+            /usr/local/bin/rsync \
+                  --times \
+                  --delete \
+                  --verbose \
+                  --compress \
+                  --recursive \
+                  --human-readable \
+                  -og --chown=${FILES_OWNER}:${FILES_GROUP} \
+                        "${SEEDBOX_USER}@${SEEDBOX_SERVER}:${remote_source}" "${local_destination}"
 
       if [ ${?} -ne 0 ]; then
-            echo "rsync ${remote_source} had no changes" && return 1
+            echo "rsync ${remote_source} failed" && return 1
       fi
 
       echo
-      echo "rsync ${remote_source} seccessful from ${SEEDBOX_USER}@${SEEDBOX_SERVER}"
+      echo "rsync ${remote_source} --> ${local_destination} successful from ${SEEDBOX_USER}@${SEEDBOX_SERVER}"
       echo
-      echo "Moving files to ${COMPLETED_SYNC_LOCATION}"
 
-      rsync --recursive \
-            -og --chown=${FILES_OWNER}:${FILES_GROUP} \
-            "${local_destination}/" "${COMPLETED_SYNC_LOCATION}"
-
-      if [ ${?} -ne 0 ]; then
-            echo "Failed moving files to ${COMPLETED_SYNC_LOCATION}" && return 1
-      fi      
-}
-
-seedbox_pull_downloaded() {
-      SYNC_TIMESTAMP="$(date "+%F_%H_%M_%S" )"
-
-      remote_source="${1}"
-      local_destination="${2}"
-
-      mkdir -p "${local_destination}" && chown ${FILES_OWNER}:${FILES_GROUP} "${local_destination}" || return 1
-
-      rsync --recursive \
-            --verbose \
-            --delete \
-            -og --chown=${FILES_OWNER}:${FILES_GROUP} \
-            --remove-source-files \
-            "${SEEDBOX_USER}@${SEEDBOX_SERVER}:${remote_source}" "${local_destination}/${SYNC_TIMESTAMP}"
-
-      if [ ${?} -ne 0 ]; then
-            echo "rsync ${SYNC_TIMESTAMP} failed" && return 1
-      fi      
-
-      echo
-      echo "rsync ${SYNC_TIMESTAMP} seccessful from ${SEEDBOX_USER}@${SEEDBOX_SERVER}"
-      echo
-      echo "Moving files to ${COMPLETED_SYNC_LOCATION}"
-
-      rsync --remove-source-files \
-            --recursive \
-            -og --chown=${FILES_OWNER}:${FILES_GROUP} \
-            "${SYNC_LOCATION}/${SYNC_TIMESTAMP}/" ${COMPLETED_SYNC_LOCATION} && \
-      rm -rf "${SYNC_LOCATION:?}/${SYNC_TIMESTAMP:?}"
-
-      if [ ${?} -ne 0 ]; then
-            echo "Failed moving ${SYNC_LOCATION}/${SYNC_TIMESTAMP}/" && return 1
-      fi      
-     
+      return 0
 }
 
 # Pull downloaded or downloaded seeding files
 # remote_source="${1}" - remote location
 # local_destination="${2}" - local destination
 
-echo "Running sync of downloaded ${SEEDBOX_DELUGE_ROOT}/${SEEDBOX_DOWNLOADING_DIR}"
-seedbox_pull_downloaded "${SEEDBOX_DELUGE_ROOT}/${SEEDBOX_DOWNLOADING_DIR}/" "${SYNC_LOCATION}"
-downloaded_success=$?
+downloaded_success=0
+seeding_success=0
 
-echo
-echo "Running sync of seeding ${SEEDBOX_DELUGE_ROOT}/${SEEDBOX_SEEDING_DIR}"
-seedbox_pull_seeding "${SEEDBOX_DELUGE_ROOT}/${SEEDBOX_SEEDING_DIR}" "${SYNC_LOCATION}/seeding"
-seeding_success=$?
+if [ "${1}" = "download" ] || [ "${1}" = "all" ]; then
+      echo "Running sync of downloaded ${SEEDBOX_DELUGE_ROOT}/${SEEDBOX_DOWNLOADING_DIR}"
+      seedbox_pull_downloaded "${SEEDBOX_DELUGE_ROOT}/${SEEDBOX_DOWNLOADING_DIR}/" "${COMPLETED_SYNC_LOCATION}"
+      downloaded_success=$?
+      echo
+fi
+
+if [ "${1}" = "seeding" ] || [ "${1}" = "all" ]; then
+      echo "Running sync of seeding ${SEEDBOX_DELUGE_ROOT}/${SEEDBOX_SEEDING_DIR}"
+      seedbox_pull_seeding "${SEEDBOX_DELUGE_ROOT}/${SEEDBOX_SEEDING_DIR}" "${SYNC_LOCATION}"
+      seeding_success=$?
+      echo
+fi
 
 echo
 
@@ -100,7 +99,7 @@ if [ ${downloaded_success} -eq 0 ] && [ ${seeding_success} -eq 0 ]; then
       exit 0
 else
       if [ ${downloaded_success} -ne 0 ]; then
-            echo "Failed sync downloaded files, or no changes on source"
+            echo "Failed sync downloaded files, or no files on source"
       fi
 
       if [ ${seeding_success} -ne 0 ]; then

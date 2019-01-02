9930ffff2a57826b0a177ce1351d7ea1088f490d
John Ramsden <johnramsden@riseup.net>
2019-01-01 22:59:36 -0800

    Add 'retry loop', no tarball

--- local/scripts/seedboxsync.sh
+++ local/scripts/seedboxsync.sh
@@ -17,137 +17,175 @@ FILES_OWNER="media"
 FILES_GROUP="media"
 
 ssh_command(){
-      OUTPUT=$(ssh "${SEEDBOX_USER}@${SEEDBOX_SERVER}" "${1}")
-      return_val="${?}"
-      echo "${OUTPUT}"
-      return "${return_val}"
+    OUTPUT=$(ssh "${SEEDBOX_USER}@${SEEDBOX_SERVER}" "${1}")
+    return_val="${?}"
+    echo "${OUTPUT}"
+    return "${return_val}"
 }
 
 move_to_temp(){
-      remote_origin="${1}"
-      remote_tempdir="${2}"
-
-      ssh_command "mkdir -p ${remote_tempdir} && mv """""${remote_origin}'*'""""" ${remote_tempdir}"
-      return ${?}
+    remote_origin="${1}"
+    remote_tempdir="${2}"
+    
+    ssh_command "mkdir -p ${remote_tempdir} && mv """""${remote_origin}'*'""""" ${remote_tempdir}"
+    return ${?}
 }
 
 move_remote_files_back(){
-      remote_tempdir="${1}"
-      remote_origin="${2}"
-      
-      echo "Moving file(s) back to ${remote_origin}"
-      ssh_command "mv ${remote_origin} ${remote_tempdir}"
-      return ${?}
-}
-
-seedbox_pull_downloaded() {
-      sync_timestamp="$(date "+%F_%H_%M_%S" )"
-      day_date="$(date +%F)"
-
-      remote_source="${1}"
-      local_destination="${2}"
-
-      # Check remote dir exists, and isn't empty
-      if ! ssh_command "[ -d ${remote_source} ]"; then
-            echo "Remote directory ${remote_source} doesn't exist"; 
-            return 1
-      fi
-
-      remote_temp_root="${SEEDBOX_TEMP_TRANSFER_ROOT}/${day_date}"
-      remote_temp_directory="${remote_temp_root}/${sync_timestamp}"
-
-      local_sync_root="${local_destination}/${sync_timestamp}"
-
-      if [ -z "$(ssh_command "ls -A ${remote_source}")" ]; then
-            echo "Remote ${remote_source} is empty, skipping sync";
-            echo
-      elif move_to_temp "${remote_source}" "${remote_temp_directory}"; then
-
-            remote_archive="${SEEDBOX_TEMP_TRANSFER_ROOT}/${day_date}/${sync_timestamp}.tar"
-
-            if ! ssh_command \
-                   "tar -cvf ${remote_archive} --directory=${remote_temp_root} ${sync_timestamp}"; then
-                echo "Failed to create archive ${remote_archive}"
-                move_remote_files_back "${remote_temp_directory}" "${remote_source}"
-                return 1
-            fi
-
-            mkdir -p "${local_sync_root}/${sync_timestamp}-extracted" && \
-            /usr/bin/lockf -s -t 0 -k "${local_sync_root}" \
-                  /usr/local/bin/rsync \
-                        --verbose \
-                        --human-readable \
-                              "${SEEDBOX_USER}@${SEEDBOX_SERVER}:${remote_archive}" \
-                              "${local_sync_root}/${sync_timestamp}.tar"
-
-            if [ ${?} -ne 0 ]; then
-                  echo "rsync ${sync_timestamp} failed"
-                  move_remote_files_back "${remote_temp_directory}" "${remote_source}"
-                  return 1
-            fi
-
-            echo "rsync ${remote_source} -->"
-            echo "${local_destination}/${sync_timestamp}"
-            echo "successful from ${SEEDBOX_USER}@${SEEDBOX_SERVER}"
-            echo
-
-            echo "Extracting archive and moving files to processing"
-            if ! tar -xvf "${local_sync_root}/${sync_timestamp}.tar" \
-                     --directory="${local_sync_root}/${sync_timestamp}-extracted"; then
-                  echo "Failed to extract transferred file"
-                  echo
-                  return 1
-            else
-                  mv "${local_sync_root}/${sync_timestamp}-extracted" \
-                     "${PROCESSING_LOCATION}" && \
-                  chown -R ${FILES_OWNER}:${FILES_GROUP} \
-                     "${PROCESSING_LOCATION}/${sync_timestamp}-extracted" && \
-                  rm -rf "${local_sync_root}" || return 1
-            fi
-
-            echo "Transfer successful, deleting tempdir ${remote_temp_directory} on remote"
-            echo
-                       
-            echo "Moved synced files to destination -->"
-            echo "${PROCESSING_LOCATION}/${sync_timestamp}-extracted"
-            echo
-      fi
-      
-      echo "Cleaning up old directories under ${SEEDBOX_TEMP_TRANSFER_ROOT}"
-      if ! [ -z "$(ssh_command "ls -A ${SEEDBOX_TEMP_TRANSFER_ROOT}")" ]; then
-      ssh_command \
-      "find """""${SEEDBOX_TEMP_TRANSFER_ROOT}/'*'""""" -maxdepth 0 -type d -not -name ${day_date} -exec rm -rv {} + ;"
-      fi
-
-      return 0     
+    remote_tempdir="${1}"
+    remote_origin="${2}"
+    
+    echo "Moving file(s) back to ${remote_origin}"
+    ssh_command "mv ${remote_origin} ${remote_tempdir}"
+    return ${?}
 }
 
 seedbox_pull_seeding() {
+    
+    remote_source="${1}"
+    local_destination="${2}"
+    
+    /usr/bin/lockf -s -t 0 -k "${local_destination}" \
+    /usr/local/bin/rsync \
+        --times \
+        --delete \
+        --verbose \
+        --compress \
+        --recursive \
+        --human-readable \
+        -og --chown=${FILES_OWNER}:${FILES_GROUP} \
+        "${SEEDBOX_USER}@${SEEDBOX_SERVER}:${remote_source}" "${local_destination}"
+    
+    if [ ${?} -ne 0 ]; then
+        echo "rsync ${remote_source} failed"
+        echo
+        return 1
+    fi
+    
+    echo "rsync ${remote_source} --> ${local_destination} successful from ${SEEDBOX_USER}@${SEEDBOX_SERVER}"
+    echo
+    
+    return 0
+}
 
-      remote_source="${1}"
-      local_destination="${2}"
+check_for_unfinished() {
+    sync_timestamp="$(date "+%F_%H_%M_%S" )"
+    local_destination="${1}"
+    pull_location="${SYNC_LOCATION}/${sync_timestamp}"
+    
+    if [ -n "$(ssh_command "ls -A ${SEEDBOX_TEMP_TRANSFER_ROOT}")" ]; then
+        
+        mkdir -p "${pull_location}" || return 1
 
-      /usr/bin/lockf -s -t 0 -k "${local_destination}" \
+        for i in 1 2 3; do
+            /usr/bin/lockf -s -t 0 -k "${pull_location}" && \
             /usr/local/bin/rsync \
-                  --times \
-                  --delete \
-                  --verbose \
-                  --compress \
-                  --recursive \
-                  --human-readable \
-                  -og --chown=${FILES_OWNER}:${FILES_GROUP} \
-                        "${SEEDBOX_USER}@${SEEDBOX_SERVER}:${remote_source}" "${local_destination}"
-
-      if [ ${?} -ne 0 ]; then
-            echo "rsync ${remote_source} failed"
-            echo
+                --times \
+                --delete \
+                --verbose \
+                --compress \
+                --recursive \
+                --human-readable \
+                -og --chown=${FILES_OWNER}:${FILES_GROUP} \
+                "${SEEDBOX_USER}@${SEEDBOX_SERVER}:${SEEDBOX_TEMP_TRANSFER_ROOT}" "${pull_location}"
+            s=${?}
+            if [ ${s} -eq 0 ]; then
+                break
+            fi
+            echo "Rsync attempt ${i} broke retrying in 150 seconds..."
+            sleep 150;
+        done
+        
+        if [ ${s} -ne 0 ]; then
+            echo "rsync ${remote_source} failed"; echo
             return 1
-      fi
+        fi
+        
+        mv "${pull_location}" "${local_destination}"
+        
+        echo "Cleaning up directories under ${SEEDBOX_TEMP_TRANSFER_ROOT}"
+        ssh_command "find """""${SEEDBOX_TEMP_TRANSFER_ROOT}/'*'""""" -delete" || return 1
+    fi
+    
+    return 0
+}
 
-      echo "rsync ${remote_source} --> ${local_destination} successful from ${SEEDBOX_USER}@${SEEDBOX_SERVER}"
-      echo
+seedbox_pull_downloads() {
+    sync_timestamp="$(date "+%F_%H_%M_%S" )"
+    day_date="$(date +%F)"
+    
+    remote_source="${1}"
+    local_destination="${2}"
+    
+    # Check remote dir exists, and isn't empty
+    if ! ssh_command "[ -d ${remote_source} ]"; then
+        echo "Remote directory ${remote_source} doesn't exist";
+        return 1
+    fi
+    
+    remote_temp_directory="${SEEDBOX_TEMP_TRANSFER_ROOT}/${day_date}/${sync_timestamp}"
+    local_temp_root="${SYNC_LOCATION}/${sync_timestamp}"
+    
+    if [ -z "$(ssh_command "ls -A ${remote_source}")" ]; then
+        echo "Remote ${remote_source} is empty, skipping sync"; echo
+    else
+        if ! move_to_temp "${remote_source}" "${remote_temp_directory}"; then
+            echo "Failed to move ${remote_source} --> ${remote_temp_directory}"; echo
+            return 1
+        fi
+        
+        mkdir -p "${local_temp_root}" || return 1
+        for i in 1 2 3; do
+            /usr/bin/lockf -s -t 0 -k "${local_temp_root}" \
+            /usr/local/bin/rsync \
+                --times \
+                --delete \
+                --verbose \
+                --compress \
+                --recursive \
+                --human-readable \
+                -og --chown=${FILES_OWNER}:${FILES_GROUP} \
+                "${SEEDBOX_USER}@${SEEDBOX_SERVER}:${remote_temp_directory}" "${local_temp_root}"
+            
+            s=${?}
+            if [ ${s} -eq 0 ]; then
+                break
+            fi
 
-      return 0
+            echo "Rsync attempt ${i} broke retrying in 150 seconds..."
+            sleep 150;
+            
+        done
+
+        if [ ${s} -ne 0 ]; then
+            echo "rsync ${remote_source} failed"; echo
+            return 1
+        fi
+        
+    fi
+    
+    echo "rsync ${remote_source} --> ${local_temp_root} successful from ${SEEDBOX_USER}@${SEEDBOX_SERVER}"
+    echo
+    if [ -n "$(ls -A ${local_temp_root})" ]; then
+        echo "Moving ${local_temp_root} --> ${local_destination}"
+        
+        if ! mkdir -p "${local_destination}" && mv "${local_temp_root}" "${local_destination}"; then
+            echo "Move failed!"
+            return 1
+        fi
+    fi
+    
+    if ! ssh_command "rm -rf ${remote_temp_directory}"; then
+        echo "Failed to delete ${remote_temp_directory} during cleanup"; echo
+        return 1
+    fi
+    
+    echo "Cleaning up empty directories under ${SEEDBOX_TEMP_TRANSFER_ROOT}"
+    if [ -n "$(ssh_command "ls -A ${SEEDBOX_TEMP_TRANSFER_ROOT}")" ]; then
+        ssh_command "find """""${SEEDBOX_TEMP_TRANSFER_ROOT}/'*'""""" -type d -empty -delete"
+    fi
+    
+    return 0
 }
 
 # Pull downloaded or downloaded seeding files
@@ -157,33 +195,45 @@ seedbox_pull_seeding() {
 downloaded_success=0
 seeding_success=0
 
+if ! check_for_unfinished "${PROCESSING_LOCATION}"; then
+    echo "Failed check for unfinished downloads"
+    exit 1
+fi
+
 if [ "${1}" = "download" ] || [ "${1}" = "all" ]; then
-      echo "Running sync of downloaded ${SEEDBOX_DELUGE_ROOT}/${SEEDBOX_DOWNLOADING_DIR}"
-      seedbox_pull_downloaded "${SEEDBOX_DELUGE_ROOT}/${SEEDBOX_DOWNLOADING_DIR}/" "${COMPLETED_SYNC_LOCATION}"
-      downloaded_success=$?
-      echo
+    echo "Running sync of downloaded ${SEEDBOX_DELUGE_ROOT}/${SEEDBOX_DOWNLOADING_DIR}"
+    seedbox_pull_downloads "${SEEDBOX_DELUGE_ROOT}/${SEEDBOX_DOWNLOADING_DIR}/" "${PROCESSING_LOCATION}"
+    downloaded_success=$?
+    echo
 fi
 
 if [ "${1}" = "seeding" ] || [ "${1}" = "all" ]; then
-      echo "Running sync of seeding ${SEEDBOX_DELUGE_ROOT}/${SEEDBOX_SEEDING_DIR}"
-      seedbox_pull_seeding "${SEEDBOX_DELUGE_ROOT}/${SEEDBOX_SEEDING_DIR}" "${SYNC_LOCATION}"
-      seeding_success=$?
-      echo
+    echo "Running sync of seeding ${SEEDBOX_DELUGE_ROOT}/${SEEDBOX_SEEDING_DIR}"
+    seedbox_pull_seeding "${SEEDBOX_DELUGE_ROOT}/${SEEDBOX_SEEDING_DIR}" "${SYNC_LOCATION}"
+    seeding_success=$?
+    echo
 fi
+
+if [ -n "$(ls -A ${PROCESSING_LOCATION})" ]; then
+    echo "Cleaning up empty directories in ${PROCESSING_LOCATION}"
+    find "${PROCESSING_LOCATION}/"* -type d -empty -delete
+fi
+
+chown -R "${FILES_OWNER}:${FILES_GROUP}" "${PROCESSING_LOCATION}"
 
 echo
 
 if [ ${downloaded_success} -eq 0 ] && [ ${seeding_success} -eq 0 ]; then
-      echo "Successful sync from ${SEEDBOX_USER}@${SEEDBOX_SERVER}"
-      exit 0
+    echo "Successful sync from ${SEEDBOX_USER}@${SEEDBOX_SERVER}"
+    exit 0
 else
-      if [ ${downloaded_success} -ne 0 ]; then
-            echo "Failed sync downloaded files, or no files on source"
-      fi
-
-      if [ ${seeding_success} -ne 0 ]; then
-            echo "Failed sync seeding files, or no changes on source"
-      fi
-
-      exit 1
+    if [ ${downloaded_success} -ne 0 ]; then
+        echo "Failed sync downloaded files, or no files on source"
+    fi
+    
+    if [ ${seeding_success} -ne 0 ]; then
+        echo "Failed sync seeding files, or no changes on source"
+    fi
+    
+    exit 1
 fi
